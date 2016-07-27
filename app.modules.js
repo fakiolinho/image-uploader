00596723d034830581b4c64c715f34268c9ccc28
Marios Fakiolas <m.fakiolas@hotmail.gr>
2016-07-28 02:26:32 +0300

    prepare the ground for videos

--- app.modules.js
+++ app.modules.js
@@ -9,17 +9,17 @@ angular
         uploaderController.$inject = ['$scope'];
 
         function uploaderController($scope) {
-            $scope.thumbnailUrl = null;
+            $scope.videoHash = null;
             $scope.options = {
                 maxFileSize: 5000000,
                 type: "POST",
                 url: 'https://upload.wistia.com?api_password=5218e005d8993be736f65a79b73329bad219771f0cbfa86ccee51bcb0acfa8d9',
-                acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
+                acceptFileTypes: /(\.|\/)(mp4|ogg)$/i
             };
 
             $scope.$on('fileuploaddone', function(e, data) {
                 if (!data.result.error) {
-                    $scope.thumbnailUrl = data.result.thumbnail.url;
+                    $scope.videoHash = data.result.hashed_id;
                 }
             });
         }
